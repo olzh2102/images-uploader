a29dd32d6b4f46bb8c65644c2faf579ea0c8d4cc
olzhas2102 <olzhas2102@yahoo.com>
2018-05-21 17:33:39 +0200

    Programmatic navigation

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,19 +1,32 @@
 <template>
   <div class="ui secondary pointing menu">
     <a href="/" class="active item">Image Storage</a>
-    <div class="right menu"><a href="#" class="ui item" @click="login">Login</a></div>
+
+    <div class="right menu">
+      <div v-if="isLoggedIn" class="horizontal">
+        <a class="item">Images</a>
+        <a class="item">Upload</a> 
+        <a class="item" @click="logout">Logout</a>
+      </div>
+      <a v-else href="#" class="ui item" @click="login">Login</a>
+    </div>
   </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapActions, mapGetters } from 'vuex'
 
 export default {
   name: 'AppHeader',
-  methods: mapActions(['login'])
+  computed: mapGetters(['isLoggedIn']),
+  methods: mapActions(['login', 'logout'])
 }
 </script>
 
-<style>
+<style scoped>
+.horizontal {
+  display: flex;
+  flex-direction: row;
+}
 
 </style>
